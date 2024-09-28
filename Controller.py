09b7e9ab064e02117ec2b62350cd4bab37f3039a
alexramos-coder <fcn_alexandre@hotmail.com>
2024-09-28 11:53:56 -0300

    Modified Controller endpoint

--- Controller.py
+++ Controller.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from codeAssistantApi.AssistantInlineService import AssistantInlineService
 
 app = Flask('__assistant__')
 
 
-@app.route('/api/assistance/inline')
-def home():
-    return "Hello World"
+@app.route('/api/assistance/inline', methods=['POST'])
+def getInlineAssistance():
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "No JSON data provided"}), 400
+    code = data.get('code')
+    service = AssistantInlineService()
+    try:
+        response = service.getInlineAssistance(code)
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error"}), 500
 
 
 app.run()
